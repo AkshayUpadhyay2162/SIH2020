67848e2a112eff90ac543fe39b95fe3ddc52fbbc
Akshay JS Upadhyay <akshayu383@gmail.com>
2020-02-06 12:51:22 +0530

    06-02-20

--- app/src/main/java/com/akshaytech/turbi/Results.java
+++ app/src/main/java/com/akshaytech/turbi/Results.java
@@ -2,12 +2,14 @@ package com.akshaytech.turbi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Results extends AppCompatActivity {
-
+   TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+        textView1 = (TextView)findViewById(R.id.tv11);
     }
 }
--- app/src/main/java/com/akshaytech/turbi/SecondActivity.java
+++ app/src/main/java/com/akshaytech/turbi/SecondActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -13,9 +15,11 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.IOException;
@@ -28,24 +32,30 @@ public class SecondActivity extends AppCompatActivity {
     Button button1,button2,button3,button4;
     ImageView imageView1,imageView2,imageView3;
     String pathtofile;
+    private  Bitmap bmp;
+
+    TextView textView1;
+    private int[][] rgbValues;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        imageView1 = (ImageView)findViewById(R.id.iv1);
-        imageView2 = (ImageView)findViewById(R.id.iv2);
-        imageView3 = (ImageView)findViewById(R.id.iv3);
+        imageView1 = (ImageView) findViewById(R.id.iv1);
+        imageView2 = (ImageView) findViewById(R.id.iv2);
+        imageView3 = (ImageView) findViewById(R.id.iv3);
+        textView1 = (TextView) findViewById(R.id.tv111);
+
         button1 = (Button) findViewById(R.id.btn_1);
-        button2 = (Button)findViewById(R.id.btn_2);
-        button3 = (Button)findViewById(R.id.btn_3);
-        button4 = (Button)findViewById(R.id.btn_result);
-        if(Build.VERSION.SDK_INT>=23){
-            requestPermissions(new String[]{Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE},2);
+        button2 = (Button) findViewById(R.id.btn_2);
+        button3 = (Button) findViewById(R.id.btn_3);
+        button4 = (Button) findViewById(R.id.btn_result);
+        if (Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
         }
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SecondActivity.this,Results.class);
+                Intent intent = new Intent(SecondActivity.this, Results.class);
                 startActivity(intent);
             }
         });
@@ -58,7 +68,7 @@ public class SecondActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-              dispatchPictureTakeAction2();
+                dispatchPictureTakeAction2();
             }
         });
         button3.setOnClickListener(new View.OnClickListener() {
@@ -68,6 +78,38 @@ public class SecondActivity extends AppCompatActivity {
             }
         });
 
+        imageView1.setOnTouchListener(new ImageView.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // TODO Auto-generated method stub
+                int x=0;
+                int y=0;
+//                textView1.setText("Touch coordinates : " +
+//                        String.valueOf(event.getX()) + "x" + String.valueOf(event.getY()));
+                ImageView imageView = ((ImageView)v);
+                Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                int pixel = bitmap.getPixel(x,y);
+                float redValue = Color.red(pixel);
+                float blueValue = Color.blue(pixel);
+                float greenValue = Color.green(pixel);
+
+                float Grayscale = (float) ((0.3*redValue)+(0.59*greenValue)+(0.11*blueValue));
+                if(Grayscale>0&&Grayscale<101.25){
+                    textView1.setText("Low Turbidity");
+                }
+                else if(Grayscale>101.25&&Grayscale<155.95){
+                    textView1.setText("Moderate Turbidity");
+                }
+                else{
+                    textView1.setText("High Turbidity");
+                }
+        /*if(redValue == 255){
+            if(blueValue == 0)
+                if(greenValue==0)
+               textViewCol.setText("It is Red");
+            }*/
+                return true;    }
+        });
     }
 
     @Override
